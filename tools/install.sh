281ee675a8a629c4f221c2ad1de45d3de7d0776d
Jonatan <jrperdomoz@gmail.com>
2015-04-10 23:56:07 -0700

    Updated install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 if [[ $1 ]]; then
-    project_name = $1
+    project_name=$1
 else
-    project_name = 'project_name'
+    project_name='project_name'
 fi
 
 git clone https://github.com/jrperdomoz/djvagrant.git $project_name
 cd $project_name
+# For fix "sed: RE error: illegal byte sequence"
+export LC_CTYPE=C
+export LANG=C
 find . -type f -print0 | xargs -0 sed -i '' -e 's/{{project_name}}/$project_name/g'
-echo "=)"
